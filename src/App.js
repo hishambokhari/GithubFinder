fc4fbb2130a95c593f60b60522de68006bacf4c5
hishambokhari <hish.b92@gmail.com>
2020-06-30 15:03:13 +0500

    Setting state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/Useritem';
 
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
+        <UserItem />
       </div>
       
     );
